6396412987a2ed86f67975ac4887a8c87c45988a
richard <richardyao75@gmail.com>
2026-09-24 03:20:01 +1000

    add cart total test and jenkins automates it

---
+++ Tests/ShoppingCartTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+public class ShoppingCartTests
+{
+    [Fact]
+    public void AddingTwoPastaPacksGivesCorrectTotal()
+    {
+        Product pasta = new Product();
+        pasta.Name = "Pasta";
+        pasta.PackSizeGrams = 500;
+
+        StoreProduct storePasta = new StoreProduct(
+            1,
+            pasta,
+            "Coals & Co",
+            2.41m);
+
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.AddItem(storePasta);
+        cart.AddItem(storePasta);
+
+        decimal actualTotal = cart.CalculateTotal();
+
+        Assert.Equal(4.82m, actualTotal);
+    }
+}
